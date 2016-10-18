304eb57fd3a1797163dd67feacc344a86399ddf9
David <David@DESKTOP-N9LTCNO>
2016-10-18 17:56:28 +0100

    coisas...................................

--- SS_OpenCV/ImageClass.cs
+++ SS_OpenCV/ImageClass.cs
@@ -144,7 +144,8 @@ namespace SS_OpenCV
                             red = dataPtr[2];
 
                             // convert to gray
-                            switch (mode) {
+                            switch (mode)
+                            {
                                 case 'M': gray = (byte)(((int)blue + green + red) / 3); break;
                                 case 'R': gray = (byte)red; break;
                                 case 'G': gray = (byte)green; break;
@@ -185,12 +186,12 @@ namespace SS_OpenCV
 
                 for (int y = 0; y < height; y++)
                 {
-                    for(int x = 0; x < width; x++)
+                    for (int x = 0; x < width; x++)
                     {
                         byte* orig = dataPtrCopy + (y - dy) * widthstep + (x - dx) * nC;
                         byte* dest = dataPtr + y * widthstep + x * nC;
 
-                        if (y - dy > 0 && y - dy < height && x - dx > 0 && x - dx < width)
+                        if (y - dy >= 0 && y - dy < height && x - dx >= 0 && x - dx < width)
                         {
                             dest[0] = orig[0];
                             dest[1] = orig[1];
@@ -310,15 +311,17 @@ namespace SS_OpenCV
                 int width = img.Width;
                 int height = img.Height;
 
-                for (int y = 1; y < height-1; y++)
+                for (int y = 1; y < height - 1; y++)
                 {
-                    for (int x = 1; x < width-1; x++)
+                    for (int x = 1; x < width - 1; x++)
                     {
                         byte* dest = dataPtr + y * widthstep + x * nC;
-                        int[] sum = {0, 0, 0};
-                        for (int j = -1; j <= 1; j++) {
-                            for (int i = -1; i <= 1; i++) {
-                                byte* orig = dataPtrCopy + (y+j) * widthstep + (x+i) * nC;
+                        int[] sum = { 0, 0, 0 };
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            for (int i = -1; i <= 1; i++)
+                            {
+                                byte* orig = dataPtrCopy + (y + j) * widthstep + (x + i) * nC;
                                 sum[0] += orig[0];
                                 sum[1] += orig[1];
                                 sum[2] += orig[2];
@@ -329,46 +332,86 @@ namespace SS_OpenCV
                         dest[2] = (byte)(sum[2] / 9);
                     }
                 }
+
+                //cantos
+                //Superior esquerdo
+                dataPtr[0] = (byte)((4 * dataPtrCopy[0] + 2 * (dataPtrCopy + widthstep)[0] + 2 * (dataPtrCopy + nC)[0] + (dataPtrCopy + widthstep + nC)[0]) / 9);
+                dataPtr[1] = (byte)((4 * dataPtrCopy[1] + 2 * (dataPtrCopy + widthstep)[1] + 2 * (dataPtrCopy + nC)[1] + (dataPtrCopy + widthstep + nC)[1]) / 9);
+                dataPtr[2] = (byte)((4 * dataPtrCopy[2] + 2 * (dataPtrCopy + widthstep)[2] + 2 * (dataPtrCopy + nC)[2] + (dataPtrCopy + widthstep + nC)[2]) / 9);
+
+                //Superior direito
+                byte* corner = dataPtr + (width - 1) * nC;
+                byte* cornerCopy = dataPtrCopy + (width - 1) * nC;
+
+                corner[0] = (byte)((4 * cornerCopy[0] + 2 * (cornerCopy + widthstep)[0] + 2 * (cornerCopy - nC)[0] + (cornerCopy + widthstep - nC)[0]) / 9);
+                corner[1] = (byte)((4 * cornerCopy[1] + 2 * (cornerCopy + widthstep)[1] + 2 * (cornerCopy - nC)[1] + (cornerCopy + widthstep - nC)[1]) / 9);
+                corner[2] = (byte)((4 * cornerCopy[2] + 2 * (cornerCopy + widthstep)[2] + 2 * (cornerCopy - nC)[2] + (cornerCopy + widthstep - nC)[2]) / 9);
+
+                //Inferior esquerdo
+                corner = dataPtr + (height - 1) * widthstep;
+                cornerCopy = dataPtrCopy + (height - 1) * widthstep;
+
+                corner[0] = (byte)((4 * cornerCopy[0] + 2 * (cornerCopy - widthstep)[0] + 2 * (cornerCopy + nC)[0] + (cornerCopy - widthstep + nC)[0]) / 9);
+                corner[1] = (byte)((4 * cornerCopy[1] + 2 * (cornerCopy - widthstep)[1] + 2 * (cornerCopy + nC)[1] + (cornerCopy - widthstep + nC)[1]) / 9);
+                corner[2] = (byte)((4 * cornerCopy[2] + 2 * (cornerCopy - widthstep)[2] + 2 * (cornerCopy + nC)[2] + (cornerCopy - widthstep + nC)[2]) / 9);
+
+                //Inferior direito
+                corner = dataPtr + (height - 1) * widthstep + (width - 1) * nC;
+                cornerCopy = dataPtrCopy + (height - 1) * widthstep + (width - 1) * nC;
+
+                corner[0] = (byte)((4 * cornerCopy[0] + 2 * (cornerCopy - widthstep)[0] + 2 * (cornerCopy - nC)[0] + (cornerCopy - widthstep - nC)[0]) / 9);
+                corner[1] = (byte)((4 * cornerCopy[1] + 2 * (cornerCopy - widthstep)[1] + 2 * (cornerCopy - nC)[1] + (cornerCopy - widthstep - nC)[1]) / 9);
+                corner[2] = (byte)((4 * cornerCopy[2] + 2 * (cornerCopy - widthstep)[2] + 2 * (cornerCopy - nC)[2] + (cornerCopy - widthstep - nC)[2]) / 9);
+
                 //margens
-                int[] sum1 = { 0, 0, 0 };
-                int[] sum2 = { 0, 0, 0 };
-                for (int y = 0; y < height; y++)
+                for (int i = 1; i < width - 1; i++)
                 {
-                    byte* leftdest = dataPtr + y * widthstep;
-                    byte* rightdest = dataPtr + y * widthstep + (width - 1) * nC;
+                    //Margem superior
+                    byte* margemSup = dataPtr + i * nC;
+                    byte* margemSupCop = dataPtrCopy + i * nC;
 
-                    for (int j = -1; j <= 1; j++)
-                    {
-                        for (int i = -1; i <= 1; i++)
-                        {
-                            int ycord = y + j;
-                            int leftx = i;
-                            int rightx = width - 1 + i;
-                            if (ycord < 0) ycord++;
-                            if (ycord >= height) ycord--;
-                            if (leftx < 0) leftx++;
-                            if (rightx >= width) rightx++;
-                            byte* leftorig = dataPtrCopy + ycord * widthstep + leftx * nC;
-                            byte* rightorig = dataPtrCopy + ycord * widthstep + rightx * nC;
+                    byte* mSupLeft = margemSupCop - nC;
+                    byte* mSupRight = margemSupCop + nC;
 
-                            sum1[0] += leftorig[0];
-                            sum1[1] += leftorig[1];
-                            sum1[2] += leftorig[2];
+                    margemSup[0] = (byte)((2 * margemSupCop[0] + 2 * mSupLeft[0] + 2 * mSupRight[0] + (margemSupCop + widthstep)[0] + (mSupRight + widthstep)[0] + (mSupLeft + widthstep)[0]) / 9);
+                    margemSup[1] = (byte)((2 * margemSupCop[1] + 2 * mSupLeft[1] + 2 * mSupRight[1] + (margemSupCop + widthstep)[1] + (mSupRight + widthstep)[1] + (mSupLeft + widthstep)[1]) / 9);
+                    margemSup[2] = (byte)((2 * margemSupCop[2] + 2 * mSupLeft[2] + 2 * mSupRight[2] + (margemSupCop + widthstep)[2] + (mSupRight + widthstep)[2] + (mSupLeft + widthstep)[2]) / 9);
 
-                            sum2[0] += rightorig[0];
-                            sum2[1] += rightorig[1];
-                            sum2[2] += rightorig[2];
-                        }
-                    }
+                    //Margem inferior
+                    byte* margemInf = dataPtr + (height - 1) * widthstep + i * nC;
+                    byte* margemInfCop = dataPtrCopy + (height - 1) * widthstep + i * nC;
 
-                    leftdest[0] = (byte)(sum1[0] / 9);
-                    leftdest[1] = (byte)(sum1[1] / 9);
-                    leftdest[2] = (byte)(sum1[2] / 9);
+                    byte* mInfLeft = margemInfCop - nC;
+                    byte* mInfRight = margemInfCop + nC;
 
-                    rightdest[0] = (byte)(sum2[0] / 9);
-                    rightdest[1] = (byte)(sum2[1] / 9);
-                    rightdest[2] = (byte)(sum2[2] / 9);
+                    margemInf[0] = (byte)((2 * margemInfCop[0] + 2 * mInfLeft[0] + 2 * mInfRight[0] + (margemInfCop - widthstep)[0] + (mInfRight - widthstep)[0] + (mInfLeft - widthstep)[0]) / 9);
+                    margemInf[1] = (byte)((2 * margemInfCop[1] + 2 * mInfLeft[1] + 2 * mInfRight[1] + (margemInfCop - widthstep)[1] + (mInfRight - widthstep)[1] + (mInfLeft - widthstep)[1]) / 9);
+                    margemInf[2] = (byte)((2 * margemInfCop[2] + 2 * mInfLeft[2] + 2 * mInfRight[2] + (margemInfCop - widthstep)[2] + (mInfRight - widthstep)[2] + (mInfLeft - widthstep)[2]) / 9);
+                }
 
+                for (int i = 1; i < height - 1; i++)
+                {
+                    //Margem esquerda
+                    byte* margemEsq = dataPtr + i * widthstep;
+                    byte* margemEsqCop = dataPtrCopy + i * widthstep;
+
+                    byte* mEsqUp = margemEsqCop - widthstep;
+                    byte* mEsqDown = margemEsqCop + widthstep;
+
+                    margemEsq[0] = (byte)((2 * margemEsqCop[0] + 2 * mEsqUp[0] + 2 * mEsqDown[0] + (margemEsqCop + nC)[0] + (mEsqDown + nC)[0] + (mEsqUp + nC)[0]) / 9);
+                    margemEsq[1] = (byte)((2 * margemEsqCop[1] + 2 * mEsqUp[1] + 2 * mEsqDown[1] + (margemEsqCop + nC)[1] + (mEsqDown + nC)[1] + (mEsqUp + nC)[1]) / 9);
+                    margemEsq[2] = (byte)((2 * margemEsqCop[2] + 2 * mEsqUp[2] + 2 * mEsqDown[2] + (margemEsqCop + nC)[2] + (mEsqDown + nC)[2] + (mEsqUp + nC)[2]) / 9);
+
+                    //Margem direita
+                    byte* margemDir = dataPtr + (width - 1) * nC + i * widthstep;
+                    byte* margemDirCop = dataPtrCopy + (width - 1) * nC + i * widthstep;
+
+                    byte* mDirUp = margemDirCop - widthstep;
+                    byte* mDirDown = margemDirCop + widthstep;
+
+                    margemDir[0] = (byte)((2 * margemDirCop[0] + 2 * mDirUp[0] + 2 * mDirDown[0] + (margemDirCop - nC)[0] + (mDirDown - nC)[0] + (mDirUp - nC)[0]) / 9);
+                    margemDir[1] = (byte)((2 * margemDirCop[1] + 2 * mDirUp[1] + 2 * mDirDown[1] + (margemDirCop - nC)[1] + (mDirDown - nC)[1] + (mDirUp - nC)[1]) / 9);
+                    margemDir[2] = (byte)((2 * margemDirCop[2] + 2 * mDirUp[2] + 2 * mDirDown[2] + (margemDirCop - nC)[2] + (mDirDown - nC)[2] + (mDirUp - nC)[2]) / 9);
                 }
             }
         }
---
+++ SS_OpenCV/WeightMatrix.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace SS_OpenCV
+{
+    partial class WeightMatrix
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weight = new System.Windows.Forms.TextBox();
+            this.bttmright = new System.Windows.Forms.TextBox();
+            this.bottom = new System.Windows.Forms.TextBox();
+            this.bttmleft = new System.Windows.Forms.TextBox();
+            this.left = new System.Windows.Forms.TextBox();
+            this.middle = new System.Windows.Forms.TextBox();
+            this.right = new System.Windows.Forms.TextBox();
+            this.topleft = new System.Windows.Forms.TextBox();
+            this.top = new System.Windows.Forms.TextBox();
+            this.topright = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // weight
+            // 
+            this.weight.Location = new System.Drawing.Point(130, 246);
+            this.weight.Name = "weight";
+            this.weight.Size = new System.Drawing.Size(60, 20);
+            this.weight.TabIndex = 0;
+            // 
+            // bttmright
+            // 
+            this.bttmright.Location = new System.Drawing.Point(157, 179);
+            this.bttmright.Name = "bttmright";
+            this.bttmright.Size = new System.Drawing.Size(30, 20);
+            this.bttmright.TabIndex = 1;
+            // 
+            // bottom
+            // 
+            this.bottom.Location = new System.Drawing.Point(121, 179);
+            this.bottom.Name = "bottom";
+            this.bottom.Size = new System.Drawing.Size(30, 20);
+            this.bottom.TabIndex = 2;
+            // 
+            // bttmleft
+            // 
+            this.bttmleft.Location = new System.Drawing.Point(85, 179);
+            this.bttmleft.Name = "bttmleft";
+            this.bttmleft.Size = new System.Drawing.Size(30, 20);
+            this.bttmleft.TabIndex = 3;
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(85, 153);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(30, 20);
+            this.left.TabIndex = 4;
+            // 
+            // middle
+            // 
+            this.middle.Location = new System.Drawing.Point(121, 153);
+            this.middle.Name = "middle";
+            this.middle.Size = new System.Drawing.Size(30, 20);
+            this.middle.TabIndex = 5;
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(157, 153);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(30, 20);
+            this.right.TabIndex = 6;
+            // 
+            // topleft
+            // 
+            this.topleft.Location = new System.Drawing.Point(85, 127);
+            this.topleft.Name = "topleft";
+            this.topleft.Size = new System.Drawing.Size(30, 20);
+            this.topleft.TabIndex = 7;
+            // 
+            // top
+            // 
+            this.top.Location = new System.Drawing.Point(121, 127);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(30, 20);
+            this.top.TabIndex = 8;
+            // 
+            // topright
+            // 
+            this.topright.Location = new System.Drawing.Point(157, 127);
+            this.topright.Name = "topright";
+            this.topright.Size = new System.Drawing.Size(30, 20);
+            this.topright.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 47);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(231, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // ok
+            // 
+            this.ok.Location = new System.Drawing.Point(32, 342);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(60, 20);
+            this.ok.TabIndex = 11;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Location = new System.Drawing.Point(183, 342);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(60, 20);
+            this.cancel.TabIndex = 12;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 249);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Weight:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Coefficients:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Filter:";
+            // 
+            // WeightMatrix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 374);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancel);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.topright);
+            this.Controls.Add(this.top);
+            this.Controls.Add(this.topleft);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.middle);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.bttmleft);
+            this.Controls.Add(this.bottom);
+            this.Controls.Add(this.bttmright);
+            this.Controls.Add(this.weight);
+            this.Name = "WeightMatrix";
+            this.Text = "WeightMatrix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox weight;
+        public System.Windows.Forms.TextBox bttmright;
+        public System.Windows.Forms.TextBox bottom;
+        public System.Windows.Forms.TextBox bttmleft;
+        public System.Windows.Forms.TextBox left;
+        public System.Windows.Forms.TextBox middle;
+        public System.Windows.Forms.TextBox right;
+        public System.Windows.Forms.TextBox topleft;
+        public System.Windows.Forms.TextBox top;
+        public System.Windows.Forms.TextBox topright;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
